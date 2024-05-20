02ad32bf7ab380f99e921dbad9620e02e80ac259
Меркин Арсен <arsen.merkin@gmail.com>
2024-05-20 17:27:16 +0500

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Incident Management API')
+    .setDescription('API documentation for Incident Management System')
+    .setVersion('1.0')
+    .addTag('incidents')
+    .addTag('users')
+    .addTag('operators')
+    .addTag('chat')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
